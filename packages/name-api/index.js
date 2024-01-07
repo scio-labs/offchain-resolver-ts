8bb6300578d3b234af8320d8d8539221821ce742
James Brown <soylentblue@hotmail.co.uk>
2024-01-07 23:17:35 +1100

    Update name server

--- packages/name-api/index.js
+++ packages/name-api/index.js
@@ -1,4 +1,22 @@
 import fastify from 'fastify';
+import { Command } from 'commander';
+import { ethers } from 'ethers';
+import { JSONDatabase } from './json';
+
+const program = new Command();
+program
+  .requiredOption(
+    '-k --private-key <key>',
+    'Private key to sign responses with. Prefix with @ to read from a file'
+  )
+  .requiredOption('-d --data <file>', 'JSON file to read data from')
+program.parse(process.argv);
+const options = program.opts();
+let privateKey = options.privateKey;
+
+const address = ethers.utils.computeAddress(privateKey);
+const signer = new ethers.utils.SigningKey(privateKey);
+const db = JSONDatabase.fromFilename(options.data, parseInt(options.ttl));
 
 const app = fastify();
 
@@ -6,9 +24,19 @@ app.get('/', async () => {
   return 'hello world';
 });
 
+app.get('/:name/:tokenId/:signature', async (request, reply) => {
+  const { name, tokenId, signature } = request.params;
+  // first check if name is taken
+  if (!db.checkAvailable(name)) {
+    return "Error: Name Unavilable";
+  }
+  
+  return { param1, param2, param3 };
+});
+
 const start = async () => {
   try {
-    await app.listen({ port: 3000 });
+    await app.listen({ port: 8083 });
     app.log.info(`Server is listening on ${app.server.address().port}`);
   } catch (err) {
     app.log.error(err);
---
+++ packages/name-api/json.ts
@@ -0,0 +1,120 @@
+
+import { readFileSync, writeFileSync } from 'fs';
+
+interface NameData {
+  addresses?: { [coinType: number]: string };
+  text?: { [key: string]: string };
+  contenthash?: string;
+}
+
+type ZoneData = { [name: string]: NameData };
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const EMPTY_CONTENT_HASH = '0x';
+
+let jFileName: string;
+
+export class JSONDatabase {
+  data: ZoneData;
+  ttl: number;
+
+  constructor(data: ZoneData, ttl: number) {
+    // Insert an empty synthetic wildcard record for every concrete name that doesn't have one
+    // This is to ensure that if '*.eth' exists and 'test.eth' exists, 'blah.test.eth' does not resolve to '*.eth'.
+    this.data = Object.assign({}, data);
+    /*for (const k of Object.keys(this.data)) {
+      if (!k.startsWith('*.') && !this.data['*.' + k]) {
+        this.data['*.' + k] = {};
+      }
+    }*/
+    this.ttl = ttl;
+  }
+
+  static fromFilename(filename: string, ttl: number) {
+    jFileName = filename;
+    return new JSONDatabase(
+      JSON.parse(readFileSync(filename, { encoding: 'utf-8' })),
+      ttl
+    );
+  }
+
+  addr(name: string, coinType: number) {
+    const nameData = this.findName(name);
+    if (!nameData || !nameData.addresses || !nameData.addresses[coinType]) {
+      return { addr: ZERO_ADDRESS, ttl: this.ttl };
+    }
+    return { addr: nameData.addresses[coinType], ttl: this.ttl };
+  }
+
+  text(name: string, key: string) {
+    const nameData = this.findName(name);
+    if (!nameData || !nameData.text || !nameData.text[key]) {
+      return { value: '', ttl: this.ttl };
+    }
+    return { value: nameData.text[key], ttl: this.ttl };
+  }
+
+  contenthash(name: string) {
+    const nameData = this.findName(name);
+    if (!nameData || !nameData.contenthash) {
+      return { contenthash: EMPTY_CONTENT_HASH, ttl: this.ttl };
+    }
+    return { contenthash: nameData.contenthash, ttl: this.ttl };
+  }
+
+  checkAvailable(name: string): boolean {
+    const fullName = name + ".smartcat.eth";
+    const nameData = this.findName(fullName);
+    if (!nameData || !nameData.text) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  addElement(name: string, address: string): string {
+    console.log("YOLESS 2");
+    const fullName = name + ".smartcat.eth";
+    const nameData = this.findName(fullName);
+    console.log("YOLESS 3 " + fullName);
+    if (!nameData || !nameData.text) {
+      
+      let text = `{
+            "addresses": {
+                "60": "${address}"
+            },
+            "contenthash": "0xe301017012204edd2984eeaf3ddf50bac238ec95c5713fb40b5e428b508fdbe55d3b9f155ffe"
+        }`;
+
+      let jsonObject = JSON.parse(text);
+      //now write to existing json
+      this.data[fullName] = jsonObject;
+
+      //save to file
+      writeFileSync(jFileName, JSON.stringify(this.data));
+
+      return "no match" ;
+
+    } else {
+      if (nameData && nameData.addresses && nameData.addresses[60]) {
+        console.log("YOLESS 5 " + fullName + " : " + nameData.addresses[60] + nameData?.text);
+      }
+      return "match";
+    }
+  }
+
+  private findName(name: string) {
+    if (this.data[name]) {
+      return this.data[name];
+    }
+
+    const labels = name.split('.');
+    for (let i = 1; i < labels.length + 1; i++) {
+      name = ['*', ...labels.slice(i)].join('.');
+      if (this.data[name]) {
+        return this.data[name];
+      }
+    }
+    return null;
+  }
+}
