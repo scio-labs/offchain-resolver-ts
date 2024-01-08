27919041e0152d0ea8c82848c6f740e5eefd2cf8
foxgem <jianhgreat@hotmail.com>
2024-01-08 15:34:56 +1100

    add dotenv.

--- packages/name-api/src/index.ts
+++ packages/name-api/src/index.ts
@@ -1,31 +1,23 @@
 // @ts-nocheck
-import fastify from 'fastify';
-import { Command } from 'commander';
-import { ethers } from 'ethers';
-import { JSONDatabase } from './json';
+import fastify from "fastify";
+import { ethers } from "ethers";
+import { JSONDatabase } from "./json";
+import { PRIVATE_KEY, JSON_FILE, TTL } from "./constants";
 
-const program = new Command();
-program
-  .requiredOption(
-    '-k --private-key <key>',
-    'Private key to sign responses with. Prefix with @ to read from a file'
-  )
-  .requiredOption('-d --data <file>', 'JSON file to read data from');
-program.parse(process.argv);
-const options = program.opts();
-const privateKey: string = options.privateKey;
-
-const address: string = ethers.computeAddress(privateKey);
-const signer: ethers.SigningKey = new ethers.SigningKey(privateKey);
-const db: JSONDatabase = JSONDatabase.fromFilename(options.data, parseInt(options.ttl, 10));
+const address: string = ethers.computeAddress(PRIVATE_KEY);
+const signer: ethers.SigningKey = new ethers.SigningKey(PRIVATE_KEY);
+const db: JSONDatabase = JSONDatabase.fromFilename(
+  JSON_FILE,
+  parseInt(TTL, 10)
+);
 
 const app = fastify();
 
-app.get('/', async () => {
-  return 'hello world';
+app.get("/", async () => {
+  return "hello world";
 });
 
-app.get('/:name/:tokenId/:signature', async (request, reply) => {
+app.get("/:name/:tokenId/:signature", async (request, reply) => {
   const { name, tokenId, signature } = request.params;
   // first check if name is taken
   if (!db.checkAvailable(name)) {
