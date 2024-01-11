e2f2bcbc51dff0a865117cbb892692dd7f3d7e9e
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2024-01-11 13:22:26 +1100

    fix to limit of entries

--- packages/name-api/src/sqlite.ts
+++ packages/name-api/src/sqlite.ts
@@ -87,7 +87,7 @@ export class SQLiteDatabase {
     // @ts-ignore
     const totalEntries = this.db.prepare('SELECT COUNT(*) as count FROM names').get().count;
 
-    if (totalEntries >= 1) {
+    if (totalEntries > 1) {
       // Decide what to do when the limit is reached, e.g., replace an existing entry or do nothing
       console.warn('Limit of 1 entrie(s) reached. Skipping addition of a new entry.');
       return;
