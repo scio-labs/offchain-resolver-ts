9d78703a2ada5a10da3342bb5b838c6df4a9a8d7
James Brown <soylentblue@hotmail.co.uk>
2024-01-17 15:41:02 +1100

    Add Avatar resolve (#7)
    * Add avatar resolve

--- packages/gateway/src/server.ts
+++ packages/gateway/src/server.ts
@@ -50,22 +50,26 @@ const queryHandlers: {
   ) => Promise<DatabaseResult>;
 } = {
   // @ts-ignore
-  'addr(bytes32)': async (dataPath, name, ttlVal, _args) => { //Main ENS route should return no address
+  'addr(bytes32)': async (dataPath, name, ttlVal, _args) => {
     return await resolve(dataPath, name, ETH_COIN_TYPE, ttlVal);
   },
   // @ts-ignore
-  'addr(bytes32,uint256)': async (dataPath, name, ttlVal, args) => { //addr(nodeHash,coinType) should return address if looking on Polygon mainnet (SLIP-44: #966).
-
+  'addr(bytes32,uint256)': async (dataPath, name, ttlVal, args) => {
     const coinType = <number>args[0];
     console.log(`${name} Attempt to resolve: ${coinType}`);
-
     return await resolve(dataPath, name, coinType, ttlVal);
   },
   // @ts-ignore
   'text(bytes32,string)': async (dataPath, name, ttlVal, args) => {
-    //const { value, ttl } = await db.text(name, args[0]);
-    const value = null;
-    return { result: [value], ttl:ttlVal };
+    try {
+      console.log(`text request: ${name} arg: ${args[0]}`);
+      const addrReq = await fetch(`${dataPath}/text/${name}/${args[0]}`);
+      const text = await addrReq.text();
+      return { result: [text], ttl:ttlVal };
+    } catch (error) {
+      console.log('error', error);
+      return { result: [""], ttl:ttlVal };
+    }
   },
   // @ts-ignore
   'contenthash(bytes32)': async (dataPath, name, ttlVal, _args) => {
--- packages/name-api/src/tokenBound.ts
+++ packages/name-api/src/tokenBound.ts
@@ -1,17 +1,20 @@
-import {TokenboundClient} from "@tokenbound/sdk";
-import {polygon, polygonMumbai} from "viem/chains";
+import { TokenboundClient } from "@tokenbound/sdk";
+import { polygon, polygonMumbai, goerli } from "viem/chains";
 
-export function getTokenBoundAccount(chainId: number, tokenContract: `0x${string}`, tokenId: `0x${string}`){
+export function getTokenBoundAccount(chainId: number, tokenContract: `0x${string}`, tokenId: `0x${string}`) {
 
 	// TODO: Add custom RPC URLS
 	let chain = undefined;
 
-	switch (chainId){
+	switch (chainId) {
 		case 80001:
 			chain = polygonMumbai;
 			break;
 		case 137:
 			chain = polygon;
+			break;
+		case 5:
+			chain = goerli;
 			break;
 	}
 
@@ -20,5 +23,31 @@ export function getTokenBoundAccount(chainId: number, tokenContract: `0x${string
 		chain
 	});
 
-	return tbaClient.getAccount({tokenContract, tokenId});
+	return tbaClient.getAccount({ tokenContract, tokenId });
+}
+
+// Can only be used if user has set up a TBA account 
+export function getTokenBoundNFT(chainIdentifier: number, accountAddress: `0x${string}`) {
+
+	let chain = undefined;
+
+	switch (chainIdentifier) {
+		case 80001:
+			chain = polygonMumbai;
+			break;
+		case 137:
+			chain = polygon;
+			break;
+		case 5:
+			chain = goerli;
+			break;
+
+	}
+
+	const tbaClient = new TokenboundClient({
+		chainId: chainIdentifier,
+		chain
+	});
+
+	return tbaClient.getNFT({ accountAddress });
 }
