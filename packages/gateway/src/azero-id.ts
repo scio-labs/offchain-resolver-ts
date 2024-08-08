32e28ccac0cb223158682fd6bdecfeb7d1f99803
Nimish Agrawal <realnimish@gmail.com>
2024-08-09 03:17:50 +0530

    feat: connect wasm contract with db

--- packages/gateway/src/azero-id.ts
+++ packages/gateway/src/azero-id.ts
@@ -1,18 +1,38 @@
+import abi from './metadata.json';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 import { Database } from './server';
+import { ContractPromise } from '@polkadot/api-contract';
+import type { WeightV2 } from '@polkadot/types/interfaces';
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 const EMPTY_CONTENT_HASH = '0x';
 
+export interface GasLimit {
+  refTime: number,
+  proofSize: number,
+}
+
 export class AzeroId implements Database {
   ttl: number;
+  contract: ContractPromise;
+  maxGasLimit: WeightV2;
 
-  constructor(ttl: number) {
+  constructor(ttl: number, contract: ContractPromise, maxGasLimit: WeightV2) {
     this.ttl = ttl;
+    this.contract = contract;
+    this.maxGasLimit = maxGasLimit;
   }
 
-  static init(ttl: number) {
+  static async init(ttl: number, providerURL: string, contractAddress: string, gasLimit: GasLimit) {
+    const wsProvider = new WsProvider(providerURL);
+    const api = await ApiPromise.create({ provider: wsProvider });
+    const contract = new ContractPromise(api, abi, contractAddress);
+    const maxGasLimit = api.registry.createType('WeightV2', gasLimit) as WeightV2;
+
     return new AzeroId(
         ttl,
+        contract,
+        maxGasLimit,
     );
   }
 
--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -2,13 +2,15 @@ import { makeApp } from './server';
 import { Command } from 'commander';
 import { readFileSync } from 'fs';
 import { ethers } from 'ethers';
-import { AzeroId } from './azero-id';
+import { AzeroId, GasLimit } from './azero-id';
 const program = new Command();
 program
   .requiredOption(
     '-k --private-key <key>',
     'Private key to sign responses with. Prefix with @ to read from a file'
   )
+  .option('--provider-url <string>', 'Provider URL of the substrate chain', 'wss://ws.test.azero.dev')
+  .option('-c --contract <string>', 'Target contract address', '5FsB91tXSEuMj6akzdPczAtmBaVKToqHmtAwSUzXh49AYzaD')
   .option('-t --ttl <number>', 'TTL for signatures', '300')
   .option('-p --port <number>', 'Port number to serve on', '8080');
 program.parse(process.argv);
@@ -21,9 +23,20 @@ if (privateKey.startsWith('@')) {
 }
 const address = ethers.utils.computeAddress(privateKey);
 const signer = new ethers.utils.SigningKey(privateKey);
-const db = AzeroId.init(parseInt(options.ttl));
-const app = makeApp(signer, '/', db);
-console.log(`Serving on port ${options.port} with signing address ${address}`);
-app.listen(parseInt(options.port));
 
-module.exports = app;
+// TODO: make it configurable
+const defaultGasLimit: GasLimit = {
+  refTime: 10_000_000_000,
+  proofSize: 1_000_000,
+};
+
+AzeroId.init(
+  parseInt(options.ttl), 
+  options.providerUrl, 
+  options.contract, 
+  defaultGasLimit,
+).then(db => {
+  const app = makeApp(signer, '/', db);
+  console.log(`Serving on port ${options.port} with signing address ${address}`);
+  app.listen(parseInt(options.port));
+});
