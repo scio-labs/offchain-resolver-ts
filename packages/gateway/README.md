6d3328e1ab256a21a8b2683f2a22cd5c1e3f9030
Dennis Zoma <dennis@scio.xyz>
2024-10-11 16:28:23 +0200

    feat: Improve & Refactor gateway for worker compatability and performance

--- packages/gateway/README.md
+++ packages/gateway/README.md
@@ -1,30 +1,33 @@
-# ENS Offchain Resolver Gateway
-This package implements a simple CCIP-read gateway server for ENS offchain resolution.
+# ENS Offchain Gateway for Aleph Zero – Gateway Server (Worker)
 
-## Usage:
-You can run the gateway as a command line tool; in its default configuration it reads the data to serve from a JSON file specified on the command line:
+> **See [README.md](../../README.md) for more information.**
 
-```
-yarn && yarn build
-yarn start --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --provider-url <url>
-```
+## Getting Started
 
-`private-key` should be an Ethereum private key that will be used to sign messages. You should configure your resolver contract to expect messages to be signed using the corresponding address.
+### Development
 
-`<url>` is the websocket endpoint of the target substrate chain (default: wss://ws.test.azero.dev)
+```bash
+# Install dependencies
+bun install
 
-## Customisation
-The JSON backend is implemented in [json.ts](src/json.ts), and implements the `Database` interface from [server.ts](src/server.ts). You can replace this with your own database service by implementing the methods provided in that interface. If a record does not exist, you should return the zero value for that type - for example, requests for nonexistent text records should be responded to with the empty string, and requests for nonexistent addresses should be responded to with the all-zero address.
+# Create `.dev.vars` & Set your private key
+cp .dev.vars.example .dev.vars
 
-For an example of how to set up a gateway server with a custom database backend, see [index.ts](src/index.ts):
-```
-const signer = new ethers.utils.SigningKey(privateKey);
+# Edit `wrangler.toml` if needed
 
-const db = JSONDatabase.fromFilename(options.data, parseInt(options.ttl));
+# Run development server
+bun run dev
 
-const app = makeApp(signer, '/', db);
-app.listen(parseInt(options.port));
+# Build for production
+bun run build
 ```
 
-## AZERO-ID implementation
-The AzeroId gateway implementation in [azero-id.ts](src/azero-id.ts) reads the state from the AZERO-ID registry contract on AlephZero network. [supported-tlds.json](src/supported-tlds.json) stores the TLDs mapped to their target registry. Update it as per need.
+### Deployment
+
+```bash
+# Put your private key in the Cloudflare secrets manager
+bunx wrangler secret put OG_PRIVATE_KEY --env <testnet|mainnet>
+
+# Deploy the worker
+bunx wrangler deploy --env <testnet|mainnet>
+```
--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,45 +1,38 @@
-import { makeApp } from './server';
-import { Command } from 'commander';
-import { readFileSync } from 'fs';
-import { ethers } from 'ethers';
-import { AzeroId, GasLimit } from './azero-id';
-import supportedTLDs from './supported-tlds.json';
+import { ethers } from 'ethers'
+import { privateKeyToAccount } from 'viem/accounts'
+import { AzeroIdResolver } from './azero-id-resolver'
+import { makeApp } from './server'
 
-const program = new Command();
-program
-  .requiredOption(
-    '-k --private-key <key>',
-    'Private key to sign responses with. Prefix with @ to read from a file'
-  )
-  .option('--provider-url <string>', 'Provider URL of the substrate chain', 'wss://ws.test.azero.dev')
-  .option('-t --ttl <number>', 'TTL for signatures', '300')
-  .option('-p --port <number>', 'Port number to serve on', '8080');
-program.parse(process.argv);
-const options = program.opts();
-let privateKey = options.privateKey;
-if (privateKey.startsWith('@')) {
-  privateKey = ethers.utils.arrayify(
-    readFileSync(privateKey.slice(1), { encoding: 'utf-8' })
-  );
+function initApp(env: any) {
+  console.log('Initializing app…')
+
+  // Destructure environment variables
+  const { OG_PRIVATE_KEY, OG_TTL, WS_PROVIDER_URL, SUPPORTED_TLDS } = env
+  if (!OG_PRIVATE_KEY || !OG_TTL || !WS_PROVIDER_URL || !Object.keys(SUPPORTED_TLDS || {}).length) {
+    throw new Error('Missing environment variables')
+  }
+
+  // Initialize the Database-like Resolver
+  const db = new AzeroIdResolver(parseInt(OG_TTL), WS_PROVIDER_URL, SUPPORTED_TLDS  )
+
+  // Initialize the CCIP-Read Handler
+  const signer = new ethers.utils.SigningKey(OG_PRIVATE_KEY)
+  const app = makeApp(signer, '/', db)
+
+  const { address } = privateKeyToAccount(OG_PRIVATE_KEY)
+  console.log(`Initialized app with signing address ${address}`)
+
+  return app
 }
-const address = ethers.utils.computeAddress(privateKey);
-const signer = new ethers.utils.SigningKey(privateKey);
 
-// TODO: make it configurable
-const defaultGasLimit: GasLimit = {
-  refTime: 10_000_000_000,
-  proofSize: 1_000_000,
-};
-
-const tldToContractAddress = new Map<string, string>(Object.entries(supportedTLDs));
-
-AzeroId.init(
-  parseInt(options.ttl), 
-  options.providerUrl, 
-  tldToContractAddress, 
-  defaultGasLimit,
-).then(db => {
-  const app = makeApp(signer, '/', db);
-  console.log(`Serving on port ${options.port} with signing address ${address}`);
-  app.listen(parseInt(options.port));
-});
+export default {
+  async fetch(request: Request, env: any) {
+    try {
+      const app = initApp(env)
+      return app.fetch(request)
+    } catch (e) {
+      console.error(e)
+      return new Response('xInternal server error', { status: 500 })
+    }
+  },
+}
